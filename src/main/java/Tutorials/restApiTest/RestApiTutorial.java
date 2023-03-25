c9c8d10f3152d815f6ab74ff73d5135395fe3b1b
Lukas Veiga <lukas.veiga@hotmail.com>
2023-03-24 23:07:55 -0300

    update rest tutorial

--- src/main/java/Tutorials/restApiTest/RestApiTutorial.java
+++ src/main/java/Tutorials/restApiTest/RestApiTutorial.java
@@ -2,14 +2,28 @@ package Tutorials.restApiTest;
 
 import com.google.gson.Gson;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 
 public class RestApiTutorial {
 
     public static void main(String[] args) throws Exception {
+
+        String apiKey;
+
+        try (InputStream input = new FileInputStream("src/main/resources/application-dev.properties")) {
+
+            Properties prop = new Properties();
+            // load a properties file
+            prop.load(input);
+
+            apiKey = prop.getProperty("apiKey");
+        }
 
         Transcript transcript = new Transcript();
         transcript.setAudio_url("https://raw.githubusercontent.com/johnmarty3/JavaAPITutorial/main/Thirsty.mp4");
@@ -18,9 +32,9 @@ public class RestApiTutorial {
 
         System.out.println(jsonRequest);
 
-        HttpRequest postRequest = (HttpRequest) HttpRequest.newBuilder()
+        HttpRequest postRequest = HttpRequest.newBuilder()
                 .uri(new URI("https://api.assemblyai.com/v2/transcript"))
-                .header("authorization", "606b7bed6ff548e99c4015755dbf6da1")
+                .header("authorization", apiKey)
                 .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
                 .build();
 
@@ -32,9 +46,9 @@ public class RestApiTutorial {
         transcript = gson.fromJson(postResponse.body(), Transcript.class);
         System.out.println(transcript.getId());
 
-        HttpRequest getRequest = (HttpRequest) HttpRequest.newBuilder()
+        HttpRequest getRequest = HttpRequest.newBuilder()
                 .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId()))
-                .header("authorization", "606b7bed6ff548e99c4015755dbf6da1")
+                .header("authorization", apiKey)
                 .build();
 
         while(true){
